27956da481df79ca7cb3f4c2bf8d6d07678e1f85
Carlos e Restrepo F <cerf_fx@yahoo.com>
2021-07-24 08:51:00 -0500

    commit 1 Java

---
+++ src/test/java/repaso/julio24/clasesysqlite/AppTest.java
@@ -0,0 +1,20 @@
+package repaso.julio24.clasesysqlite;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
